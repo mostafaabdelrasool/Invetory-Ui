22f93f49f4e1a6815791616596d804164505b2bb
Maryam Mamdouh <maryammamdouh94@gmail.com>
2019-03-11 22:44:51 +0200

    add customer in order

--- src/app/orders/add-order/component/customer.order/customer.order.component.ts
+++ src/app/orders/add-order/component/customer.order/customer.order.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { EntityState } from '@ngrx/entity';
 import { Store, select } from '@ngrx/store';
 import { Customer } from 'src/app/model';
@@ -6,6 +6,9 @@ import { getCustomerState } from 'src/app/customer/store/selectors/customer.sele
 import { Observable } from 'rxjs';
 import { GenericAction, GenericActionTypes } from 'src/app/generic.store/actions/generic.actions';
 import { ReducerNames } from 'src/app/core/reducer.enum';
+import { EditpopupService } from 'src/app/shared/table/editPopup/edit.Popup.service';
+import { CustomerSetting } from 'src/app/customer/customer.setting';
+import { PopupFields } from 'src/app/shared/table/model/popup.fields';
 
 @Component({
   selector: 'app-customer-order',
@@ -14,20 +17,30 @@ import { ReducerNames } from 'src/app/core/reducer.enum';
 })
 export class CustomerOrderComponent implements OnInit {
   customer$: Observable<Customer[]>;
-  @Output() model=new EventEmitter<string>();
+  @Output() model = new EventEmitter<string>();
   selectedCustomer: string;
-  constructor(public store: Store<EntityState<Customer>>) {
-    this.customer$ = store.pipe(select(getCustomerState))
-   }
+  popupFieldSetting: PopupFields[]
+  constructor(public store: Store<EntityState<Customer>>,
+    private editpopupServiceService: EditpopupService) {
+    this.customer$ = store.pipe(select(getCustomerState));
+    this.popupFieldSetting = CustomerSetting.TableSetting.popupFields;
+  }
 
   ngOnInit() {
     this.load();
   }
   load(): any {
-    let p = new GenericAction(GenericActionTypes.Load,ReducerNames.Customer, null, 'api/customer');
+    let p = new GenericAction(GenericActionTypes.Load, ReducerNames.Customer, null, 'api/customer');
     this.store.dispatch(p);
   }
-  customerChanged(){
+  customerChanged() {
     this.model.emit(this.selectedCustomer)
+  }
+  addCustomer() {
+    this.editpopupServiceService.openDialog(this.popupFieldSetting).subscribe(x => {
+      if (x) {
+
+      }
+    })
   }
 }
